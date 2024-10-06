21018dc4b06339296190788fd38976ef05fc44f6
Vijaykarthik-S <131847128+Vijaykarthik-S@users.noreply.github.com>
2024-10-06 20:49:23 +0530

    Task 1 and 2 submission

---
+++ C# Assignment-SIS/Task 1 & 2- SIS/Payment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentInfoSystem.Entity
+{
+    public class Payment
+    {
+
+        public int PaymentID { get; set; }
+        public int StudentID { get; set; } // Foreign key to Student
+        public decimal Amount { get; set; }
+        public DateTime PaymentDate { get; set; }
+
+        public Payment(int paymentID, int studentID, decimal amount, DateTime paymentDate)
+        {
+            PaymentID = paymentID;
+            StudentID = studentID;
+            Amount = amount;
+            PaymentDate = paymentDate;
+        }
+    }
+}
