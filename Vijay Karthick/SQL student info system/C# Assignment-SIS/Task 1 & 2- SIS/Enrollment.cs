f14fa736722ff8a22adb1bccfd0eeab68d526e38
Vijaykarthik-S <131847128+Vijaykarthik-S@users.noreply.github.com>
2024-10-07 12:16:16 +0530

    Add files via upload

---
+++ Vijay Karthick/SQL student info system/C# Assignment-SIS/Task 1 & 2- SIS/Enrollment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentInfoSystem.Entity
+{
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+        public int StudentID { get; set; } // Foreign key to Student
+        public int CourseID { get; set; }  // Foreign key to Course
+        public DateTime EnrollmentDate { get; set; }
+
+        public Enrollment(int enrollmentID, int studentID, int courseID, DateTime enrollmentDate)
+        {
+            EnrollmentID = enrollmentID;
+            StudentID = studentID;
+            CourseID = courseID;
+            EnrollmentDate = enrollmentDate;
+        }
+    }
+}
