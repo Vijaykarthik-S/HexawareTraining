049bc40c19a11a6cd4905c17ff9dcc6da22ee0eb
Vijaykarthik-S <131847128+Vijaykarthik-S@users.noreply.github.com>
2024-10-07 12:06:19 +0530

    Add files via upload

---
+++ Vijay Karthick/SQL student info system/C# Assignment-SIS/Task 1 & 2- SIS/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentInfoSystem.Entity
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public Student(int studentID, string firstName, string lastName, DateTime dateOfBirth, string email, string phoneNumber)
+        {
+            StudentId = studentID;
+            FirstName = firstName;
+            LastName = lastName;
+            DateOfBirth = dateOfBirth;
+            Email = email;
+            PhoneNumber = phoneNumber;
+        }
+    }
+}
