843ed39d3d5d0f59b7db920f0e2a755b578e63fd
Vijaykarthik-S <131847128+Vijaykarthik-S@users.noreply.github.com>
2024-10-10 09:25:24 +0530

    Add files via upload

---
+++ StudentInformationSystem/Task 6 and 7/StudentInfoSystem.entity/DatabaseConnectivity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentInformationSystem
+
+{
+    public class DatabaseConnectivity
+    {
+        public static SqlConnection GetDBConnection()
+        {
+            string connectionString = "Data Source=LAPTOP-2H2P4CPV;Initial Catalog=SISDB;Integrated Security=True;Encrypt=False";
+            SqlConnection connection = new SqlConnection(connectionString);
+            return connection;
+        }
+    }
+}
