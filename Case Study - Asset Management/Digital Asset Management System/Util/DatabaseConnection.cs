838c3865f1d31a9e81ea646a97e829dc5de23224
Vijaykarthik-S <131847128+Vijaykarthik-S@users.noreply.github.com>
2024-10-12 21:23:53 +0530

    case study completed

---
+++ Case Study - Asset Management/Digital Asset Management System/Util/DatabaseConnection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digital_Asset_Management.util
+{
+    public static class DbUtil
+    {
+        public static SqlConnection GetDBConnection()
+        {
+            string connectionString = "Data Source=LAPTOP-7PTI9LUL;Initial Catalog=ASSET MANAGEMENT ;Integrated Security=True;";
+            SqlConnection connection = new SqlConnection(connectionString);
+            return connection;
+        }
+
+    }
+}
