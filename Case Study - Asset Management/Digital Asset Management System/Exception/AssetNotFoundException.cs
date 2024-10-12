699604dea205209afd30b3506f1e2fd5f8da2de0
Vijaykarthik-S <131847128+Vijaykarthik-S@users.noreply.github.com>
2024-10-12 21:26:44 +0530

    exception handling

---
+++ Case Study - Asset Management/Digital Asset Management System/Exception/AssetNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalAssetManagement.Exception
+{
+    public class AssetNotFoundException : System.Exception
+    {
+        public AssetNotFoundException(string message) : base(message) { }
+
+        public AssetNotFoundException(string message, System.Exception innerException)
+            : base(message, innerException) { }
+    }
+}
---
+++ Case Study - Asset Management/Digital Asset Management System/Exception/AssetNotMaintainedException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalAssetManagement.Exception
+{
+    public class AssetNotMaintainException : System.Exception
+    {
+        // Constructor that takes a message
+        public AssetNotMaintainException(string message)
+            : base(message) // Ensure this is correct
+        {
+        }
+
+        // Constructor that takes a message and an inner exception
+        public AssetNotMaintainException(string message, System.Exception innerException)
+            : base(message, innerException) // Ensure this is correct
+        {
+        }
+    }
+
+}
