56ba6bdcc3a43dc3de70e75dcb266df8d43bc22d
Vijaykarthik-S <131847128+Vijaykarthik-S@users.noreply.github.com>
2024-10-12 21:26:03 +0530

    Add files via upload

---
+++ Case Study - Asset Management/Digital Asset Management System/Entity/AssetAllocation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalAssetManagement.Entity
+{
+    public class AssetAllocation
+    {
+        public int AllocationId { get; set; }
+        public int AssetId { get; set; }
+        public int EmployeeId { get; set; }
+        public string AllocationDate { get; set; }
+        public string ReturnDate { get; set; }
+    }
+}
