63056661787f14f50a82a5d3bf97458c9ce8132f
Vijaykarthik-S <131847128+Vijaykarthik-S@users.noreply.github.com>
2024-10-14 22:12:04 +0530

    entity folder

---
+++ Case Study - Asset Management/Digital Asset Management System C#/Digital Asset Management System.Entity/Asset.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalAssetManagement.Entity
+{
+    public class Asset
+    {
+        public int AssetId { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string SerialNumber { get; set; }
+        public string PurchaseDate { get; set; }
+        public string Location { get; set; }
+        public string Status { get; set; }
+        public int OwnerId { get; set; }
+    }
+}
---
+++ Case Study - Asset Management/Digital Asset Management System C#/Digital Asset Management System.Entity/Reservation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalAssetManagement.Entity
+{
+    public class Reservation
+    {
+        public int ReservationId { get; set; }
+        public int AssetId { get; set; }
+        public int EmployeeId { get; set; }
+        public string ReservationDate { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+        public string Status { get; set; }
+    }
+}
