bfb28d1b560886aa65471f01ea6e3ba7c4b5d7f0
Vijaykarthik-S <131847128+Vijaykarthik-S@users.noreply.github.com>
2024-10-14 22:15:26 +0530

    Add files via upload

---
+++ Case Study - Asset Management/Digital Asset Management System C#/Digital Asset Management System.Exceptions/EmployeeNotFoundException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalAssetManagement.Exception
+{
+    public class EmployeeNotFoundException : System.Exception
+    {
+
+        public EmployeeNotFoundException() : base("Employee not found!")
+        {
+
+        }
+        public EmployeeNotFoundException(string message) : base(message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public EmployeeNotFoundException(string message, System.Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+
+}
