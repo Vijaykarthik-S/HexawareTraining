538a21952ad4bc486222b09f6e53a7ab886b6415
Vijaykarthik-S <131847128+Vijaykarthik-S@users.noreply.github.com>
2024-10-14 22:20:54 +0530

    Add files via upload

---
+++ Case Study - Asset Management/Digital Asset Management System C#/Digital Asset Management System.Util/DatabaseConnection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace DigitalAssetManagement.Util
+{
+    public static class DBUtil
+    {
+        readonly static SqlConnection sqlConnection;
+
+        static DBUtil()
+        {
+            sqlConnection = new SqlConnection();
+            sqlConnection.ConnectionString = ConfigurationManager.ConnectionStrings["MyDBConnection"].ConnectionString;
+        }
+
+        public static SqlConnection SqlConnection => sqlConnection;
+
+        public static void CloseConnection()
+        {
+            if (sqlConnection != null && sqlConnection.State == System.Data.ConnectionState.Open)
+            {
+                sqlConnection.Close();
+                
+            }
+        }
+    }
+}
