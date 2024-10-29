de635ae1896a375cd22c36094201a20916d82498
Vijaykarthik-S <131847128+Vijaykarthik-S@users.noreply.github.com>
2024-10-29 09:16:20 +0530

    Add files via upload

---
+++ Daily tasks/day 5/Library.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInversionPrinciple
+{
+    public class Library
+    {
+        public int Totalbooks;
+        public void Booksborrow(int count) {
+            if (Totalbooks >= count)
+            {
+                Totalbooks -= count;
+                Console.WriteLine($"Books borrowed is {count}\n Remaining books {Totalbooks}");
+
+            }
+            else { Console.WriteLine("Insufficient amount of books"); }
+        }
+        public void ReserveBooks(int count) {
+            Console.WriteLine($"Books reserved by Teacher {count}");
+        }
+        public void Addbooks(int count) {
+            Totalbooks += count;
+            Console.WriteLine($"Books added by librarian {count}\n Remaining books{Totalbooks}");
+        }
+    }
+}
---
+++ Daily tasks/day 5/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DependencyInversionPrinciple;
+
+
+namespace DependencyInversionPrinciple {
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Library library = new Library();
+            Console.WriteLine("Books added are : \n");
+            library.Addbooks(10);
+
+            Console.WriteLine("Books borrowed are : \n");
+            library.Booksborrow(5);
+
+            Console.WriteLine("Books reserved are : \n");
+            library.ReserveBooks(2);
+
+            Console.ReadLine();
+            
+
+        }
+    }
+}
